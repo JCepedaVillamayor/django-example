beeb978301ec9daaf9f4dc262c799ac35b0e4b77
jcepeda <jcepedavillamayor@gmail.com>
2018-11-02 11:23:10 +0100

    Improve fake data Add command to generate random users with random data

---
+++ django_example/addresses/management/commands/generate_fake_data.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...test.factories import AddressFactory
+
+
+class Command(BaseCommand):
+    help = "generates fake users to list in the application"
+
+    def add_arguments(self, parser):
+        parser.add_argument("n_of_entries", type=int)
+
+    def handle(self, *args, **options):
+        AddressFactory.create_batch(options["n_of_entries"])
+
--- django_example/addresses/test/factories.py
+++ django_example/addresses/test/factories.py
@@ -9,9 +9,19 @@ class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
 
+    username = factory.Faker("user_name")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = factory.Faker("email")
+    password = "foobarfoobar"
+
 
 class AddressFactory(DjangoModelFactory):
     class Meta:
         model = Address
 
     user = factory.SubFactory(UserFactory)
+    address = factory.Faker("address")
+    city = factory.Faker("city")
+    state = factory.Faker("state")
+    zipcode = factory.Faker("zipcode")
